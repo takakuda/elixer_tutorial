984bdf084f480280758d340bf54b63b5f4ccc2f9
kei <kei.takakuda.0415@icloud.com>
2019-04-18 02:37:20 +0900

    fizz_buzz

--- first_steps/handle_open.exs
+++ first_steps/handle_open.exs
@@ -4,3 +4,13 @@ handle_open = fn
 end
 
 IO.puts handle_open.(File.open("test"))
+
+fizz_buzz = fn
+  (0, 0) -> IO.puts("FizzBuzz")
+  (0, _) -> IO.puts("Fizz")
+  (_, 0) -> IO.puts("Buzz")
+end
+
+IO.puts fizz_buzz.(0, 0)
+IO.puts fizz_buzz.(0, 1)
+IO.puts fizz_buzz.(1, 0)
